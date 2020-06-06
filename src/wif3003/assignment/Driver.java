23bea71e49e15fed99316054deee42bd895d933a
Aliya Fatihah <aliyafthh@gmail.com>
2020-06-06 12:34:57 +0800

    getting user input

--- src/wif3003/assignment/Driver.java
+++ src/wif3003/assignment/Driver.java
@@ -1,12 +1,58 @@
 
 package wif3003.assignment;
 
+import java.util.Scanner;
+
 public class Driver {
 
     public static void main(String[] args) {
-        Game game = new Game(20000, 4, 500);
+        
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number of coordinates : ");
+        String x = input.next();
+        int n = convertToInt(x);
+        
+        System.out.println("Please enter the number of threads (has to be smaller than number of coordinates) : ");
+        String y = input.next();
+        int t = convertToInt(y);
+        boolean k = compare(n,t);
+        
+        while(k == true){
+            System.out.println("Invalid number");
+            System.out.println("Please re-enter the number of threads (has to be larger than number of coordinates) : ");
+            y = input.next();
+            t = convertToInt(y);
+            k = compare(n,t);
+        }
+        
+        System.out.println("Please enter the time limit :");
+        String z = input.next();
+        int m = convertToInt(y);
+        
+        
+        
+        Game game = new Game(n, t, m);
         game.generate1();
 //        game.generate2();
+    }
+    
+    
+    public static int convertToInt(String x){
+        
+        int y = Integer.parseInt(x);       
+        
+        return y;
+    }
+    
+    public static boolean compare(int n, int t){
+        
+        if(t > n){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
     
 }
