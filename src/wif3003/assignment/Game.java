34b86749cf693825fbc634a9afb93f59989de8c6
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2020-06-06 22:03:49 +0800

    Update Game.java

--- src/wif3003/assignment/Game.java
+++ src/wif3003/assignment/Game.java
@@ -23,43 +23,6 @@ public class Game {
         this.m = m;
         points = new ArrayList<>();
     }
-    
-    public void generate1() {
-        long startTime = System.nanoTime();
-        
-        this.generatePoints(n);
-        
-        long endTime = System.nanoTime();
-        long totalTime = endTime - startTime;
-        try {
-            File f = new File("./algo_1.txt");
-            BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
-            bw.append("Total time for algo 1 (n=" + n + "): " + String.valueOf(totalTime / 
-                    Math.pow(10, 9)) + "\n");
-            bw.close();
-        } catch (IOException e) {
-            System.out.println("Cannot read file");
-        }
-        System.out.println("\n" + (totalTime / Math.pow(10, 9)));
-    }
-    public void generate2() {
-        long startTime = System.nanoTime();
-        
-        this.generatePoints2(n);
-        
-        long endTime = System.nanoTime();
-        long totalTime = endTime - startTime;
-        try {
-            File f = new File("./algo_1.txt");
-            BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
-            bw.append("Total time for algo 2 (n=" + n + "): " + String.valueOf(totalTime / 
-                    Math.pow(10, 9)) + "\n");
-            bw.close();
-        } catch (IOException e) {
-            System.out.println("Cannot read file");
-        }
-        System.out.println("\n" + (totalTime / Math.pow(10, 9)));
-    }
 
     private void generatePoints(int n) {
         int rightLimit = 500;
@@ -77,34 +40,6 @@ public class Game {
         }
         
         Collections.shuffle(listOfNumbers);
-        for (int j = 0; j < this.n; j++) {
-            int indexForX = new Random().nextInt(listOfNumbers.size());
-            int indexForY = new Random().nextInt(listOfNumbers.size());
-            double x = listOfNumbers.get(indexForX);
-            double y = listOfNumbers.get(indexForY);
-//            System.out.printf("(%d, %d)%n", indexForX, indexForY);
-            System.out.println(new Point(x, y).toString());
-            
-            points.add(new Point(x, y));
-        }
-    }
-    
-    private void generatePoints2(int n) {
-        int rightLimit = 500;
-        int leftLimit = -500;
-        ArrayList<Double> listOfNumbers = new ArrayList<>();
-        
-        for (int i = 0; i < 5000; i++) {
-            double generatedFloat = 0;
-            while (listOfNumbers.contains(generatedFloat)) {
-                generatedFloat = leftLimit + new Random().nextFloat() * (rightLimit-(leftLimit));
-                generatedFloat = Math.round(generatedFloat * 100.00) / 100.00;
-            }
-            listOfNumbers.add(generatedFloat);
-        }
-        
-        Collections.shuffle(listOfNumbers);
-        
         for (int j = 0; j < this.n; j++) {
             int indexForX = new Random().nextInt(listOfNumbers.size());
             int indexForY = new Random().nextInt(listOfNumbers.size());
