2c362e958ed0018a79dbb4418e3c661ea0375d17
YeshwiniR <34685390+YeshwiniR@users.noreply.github.com>
2020-06-11 06:06:20 +0800

    keep track of threads

---
+++ build/classes/wif3003/assignment/MyThreadPool.java
@@ -0,0 +1,38 @@
+public class MyThreadPool
+{
+    private IList<Thread> _threads;
+    private readonly int MAX_THREADS = 10;
+
+    public MyThreadPool()
+    {
+        _threads = new List<Thread>();
+    }
+
+    public void LaunchThreads()
+    {
+        for (int i = 0; i < MAX_THREADS;i++)
+        {
+            Thread thread = new Thread(ThreadEntry);
+            thread.IsBackground = true;
+            thread.Name = string.Format("MyThread{0}",i);
+
+            _threads.Add(thread);
+            thread.Start();
+        }
+    }
+
+    public void KillThread(int index)
+    {
+        string id = string.Format("MyThread{0}",index);
+        foreach (Thread thread in _threads)
+        {
+            if (thread.Name == id)
+                thread.Abort();
+        }
+    }
+
+    void ThreadEntry()
+    {
+
+    }
+}
